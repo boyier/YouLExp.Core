a215dc79bceeb9d0416aa493ad65abc955057e28
尹鑫竹 <wohaom12@hotmail.com>
2021-04-14 17:06:32 +0800

    完善时间扩展类

--- YouLExp.Core/DateTimeExtend.cs
+++ YouLExp.Core/DateTimeExtend.cs
@@ -15,14 +15,200 @@ namespace YouLExp.Core
         /// <param name="dateTime">原格式</param>
         /// <param name="format"></param>
         /// <returns></returns>
-        internal static DateTime ToConvetDateTime(this object dateTime, string format = "yyyy-MM-dd HH:mm:ss")
+        public static DateTime ToConvetDateTime(this object dateTime, string format = "yyyy-MM-dd HH:mm:ss")
         {
-            DateTime newTime;
-            if (dateTime != null && DateTime.TryParseExact(dateTime.ToString(), format, null, System.Globalization.DateTimeStyles.None, out newTime))
+            if (dateTime != null && DateTime.TryParseExact(dateTime.ToString(), format, null, System.Globalization.DateTimeStyles.None, out DateTime newTime))
                 return newTime;
             else
                 return DateTime.MaxValue;
         }
+
+        /// <summary>
+        /// 返回当前月的第一天（01）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToMonthOfFirst(this DateTime dateTime)
+        {
+            return dateTime.AddDays(1 - dateTime.Day);
+        }
+
+        /// <summary>
+        /// 返回当前月的最后一天（28、29、30、31）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToMonthOfFast(this DateTime dateTime)
+        {
+            return dateTime.AddDays(1 - dateTime.Day).AddMonths(1).AddDays(-1);
+        }
+
+        /// <summary>
+        /// 返回本周的第一天（星期一）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToWeekOfFirst(this DateTime dateTime)
+        {
+            DateTime firstDate = DateTime.Now;
+            switch (dateTime.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    firstDate = dateTime;
+                    break;
+                case DayOfWeek.Tuesday:
+                    firstDate = dateTime.AddDays(-1);
+                    break;
+                case DayOfWeek.Wednesday:
+                    firstDate = dateTime.AddDays(-2);
+                    break;
+                case DayOfWeek.Thursday:
+                    firstDate = dateTime.AddDays(-3);
+                    break;
+                case DayOfWeek.Friday:
+                    firstDate = dateTime.AddDays(-4);
+                    break;
+                case DayOfWeek.Saturday:
+                    firstDate = dateTime.AddDays(-5);
+                    break;
+                case DayOfWeek.Sunday:
+                    firstDate = dateTime.AddDays(-6);
+                    break;
+            }
+            return firstDate;
+        }
+
+        /// <summary>
+        /// 返回本周的最后一天（星期日）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToWeekOfFast(this DateTime dateTime)
+        {
+            return dateTime.AddDays(7 - (int)dateTime.DayOfWeek);
+        }
+
+        /// <summary>
+        /// 获取本季度的第一天
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToSeasonOfFirst(this DateTime dateTime)
+        {
+            return dateTime.AddMonths(0 - (dateTime.Month - 1) % 3).AddDays(1 - dateTime.Day);
+        }
+
+        /// <summary>
+        /// 获取本季度的最后一天
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToSeasonOfFast(this DateTime dateTime)
+        {
+            return dateTime.AddMonths(0 - (dateTime.Month - 1) % 3).AddDays(1 - dateTime.Day).AddMonths(3).AddDays(-1);
+        }
+
+        /// <summary>
+        /// 获取本年年初（1月1号）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToYearOfFirst(this DateTime dateTime)
+        {
+            return dateTime.AddMonths(1 - dateTime.Month).AddDays(1 - dateTime.Day);
+        }
+
+        /// <summary>
+        /// 获取本年年末（12月31日）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToYearOfFast(this DateTime dateTime)
+        {
+            return dateTime.AddMonths(1 - dateTime.Month).AddDays(1 - dateTime.Day).AddYears(1).AddDays(-1);
+        }
+
+        /// <summary>
+        /// 获取当天的凌晨（00:00:00.000）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToDayOfFirst(this DateTime dateTime)
+        {
+            return dateTime.Date;
+        }
+
+        /// <summary>
+        /// 获取当天最后时间（23:59:59.999）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ToDayOfFast(this DateTime dateTime)
+        {
+            return dateTime.Date.AddDays(1).AddMilliseconds(-1);
+        }
+
+        #region 时间戳
+
+        /// <summary>
+        /// 获取当前时间的时间戳（13位）
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static long ToTimeStamp(this DateTime dateTime)
+        {
+            DateTime startTime = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 0, 0, 0, 0), TimeZoneInfo.Local);
+            return (dateTime.Ticks - startTime.Ticks) / 10000;   //除10000调整为13位      
+        }
+
+        /// <summary>        
+        /// 将<see cref="long"/>类型的时间戳转为<see cref="DateTime"/>     
+        /// </summary>        
+        /// <param name="timeStamp">时间戳</param>        
+        /// <returns></returns>        
+        public static DateTime ToTimeStampOfDateTime(long timeStamp)
+        {
+            DateTime dtStart = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 0, 0, 0, 0), TimeZoneInfo.Local);
+            long lTime = timeStamp * 10000;
+            TimeSpan toNow = new TimeSpan(lTime);
+            return dtStart.Add(toNow);
+        }
+
+        #endregion
+
+        #region DateTimeOffset
+
+        /// <summary>
+        /// 将<see cref=" DateTime"/>转换为<see cref="DateTimeOffset"/>
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="kind">Specifies whether a <see cref="DateTime"/> object represents a local time, a Coordinated Universal Time (UTC), or is not specified as either local time or UTC.</param>
+        /// <returns></returns>
+        public static DateTimeOffset ToDateTimeOffset(this DateTime dateTime, DateTimeKind kind = DateTimeKind.Utc)
+        {
+            return DateTime.SpecifyKind(dateTime, kind);
+        }
+
+        /// <summary>
+        /// 将<see cref="DateTimeOffset"/>转换为UTC时间<see cref="DateTime"/>
+        /// </summary>
+        /// <param name="dateTimeOffset"></param>
+        /// <returns></returns>
+        public static DateTime ToUtcDateTime(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.UtcDateTime;
+        }
+
+        /// <summary>
+        /// 将<see cref="DateTimeOffset"/>转换为<see cref="DateTime"/>
+        /// </summary>
+        /// <param name="dateTimeOffset"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.DateTime;
+        }
+        #endregion
 
     }
 }
--- YouLExp.Core/ValidExtend.cs
+++ YouLExp.Core/ValidExtend.cs
@@ -30,5 +30,59 @@ namespace YouLExp.Core
         {
             return new Regex(@"^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$").IsMatch(text);
         }
+
+        /// <summary>
+        /// 验证字符串是否符合强密码格式（必须包含大小写字母和数字的组合，不能使用特殊字符，长度在 8-10 之间）
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsValidPassword(this string text)
+        {
+            return new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,10}$").IsMatch(text);
+        }
+
+        /// <summary>
+        /// 验证字符串是否符合强密码格式（必须包含大小写字母和数字的组合，可以使用特殊字符，长度在8-10之间）
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsValidPasswordEx(this string text)
+        {
+            return new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$").IsMatch(text);
+        }
+
+        /// <summary>
+        /// 正则表达式校验
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="reg"></param>
+        /// <returns></returns>
+        public static bool IsRegexMatch(this string text, string reg)
+        {
+            return IsRegexMatch(text, new Regex(reg));
+        }
+
+        /// <summary>
+        /// 正则表达式校验
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="reg"></param>
+        /// <param name="regex"></param>
+        /// <returns></returns>
+        public static bool IsRegexMatch(this string text, string reg, RegexOptions regex)
+        {
+            return IsRegexMatch(text, new Regex(reg, regex));
+        }
+
+        /// <summary>
+        /// 正则表达式校验
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="regex"></param>
+        /// <returns></returns>
+        public static bool IsRegexMatch(this string text, Regex regex)
+        {
+            return regex.IsMatch(text);
+        }
     }
 }
