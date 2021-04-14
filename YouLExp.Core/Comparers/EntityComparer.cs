b1a158b548a4170b77510df3f44d3f7d3686bc2e
尹鑫竹 <wohaom12@hotmail.com>
2021-04-14 16:28:59 +0800

    完善列表扩展类

---
+++ YouLExp.Core/Comparers/EntityComparer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouLExp.Core.Comparers
+{
+    /// <summary>
+    /// 通用的实体类比较器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    public class EntityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, object> _func;
+        private readonly Func<T, T, bool> _funcComp;
+
+        /// <summary>
+        /// 通用的实体类比较器
+        /// </summary>
+        /// <param name="predicate">多条件筛选</param>
+        public EntityComparer(Func<T, T, bool> predicate)
+        {
+            this._funcComp = predicate ?? throw new Exception("Func<T, T, bool> predicate 不能为Null。");
+        }
+
+        /// <summary>
+        /// 通用的实体类比较器
+        /// </summary>
+        /// <param name="predicate">筛选条件</param>
+        public EntityComparer(Func<T, object> predicate)
+        {
+            this._func = predicate ?? throw new Exception("Func<T, object> predicate 不能为Null。");
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(T x, T y)
+        {
+            if (this._funcComp != null)
+                return this._funcComp.Invoke(x, y);
+            else
+                return this._func.Invoke(x).Equals(this._func.Invoke(y));
+        }
+
+        /// <inheritdoc/>
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
--- YouLExp.Core/ListExtend.cs
+++ YouLExp.Core/ListExtend.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using YouLExp.Core.Comparers;
 
 namespace YouLExp.Core
 {
-    public class ListExtend
+    /// <summary>
+    /// 集合列表扩展类
+    /// </summary>
+    public static class ListExtend
     {
+        /// <summary>
+        /// 对<see cref="IEnumerable{T}"/>实体类集合按条件去重处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="predicate">过滤条件</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Distinct<T>(this IEnumerable<T> list, Func<T, object> predicate)
+        {
+            return list.Distinct(new EntityComparer<T>(predicate));
+        }
+
+        /// <summary>
+        /// 对<see cref="IEnumerable{T}"/>实体类集合按条件去重处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="multipPredicate">多条件过滤</param>
+        /// <returns></returns>
+        public static IEnumerable<T> Distinct<T>(this IEnumerable<T> list, Func<T, T, bool> multipPredicate)
+        {
+            return list.Distinct(new EntityComparer<T>(multipPredicate));
+        }
+
     }
 }
